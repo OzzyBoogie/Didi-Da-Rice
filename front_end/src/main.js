53ef39f5dee31adda3bdaf133409bf3be54c2168
OzzyHelium <ozzyboogie@outlook.com>
2023-11-02 18:21:02 +0800

    init frontend

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App).mount('#app')
+app.use(ElementPlus)
