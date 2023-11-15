9386d5f0732fa7bbcf6d8f24683c3f69a2f3b1a8
OzzyHelium <ozzyboogie@outlook.com>
2023-11-15 18:39:29 +0800

    主界面3.1

--- front_end/src/views/MainWindow.vue
+++ front_end/src/views/MainWindow.vue
@@ -10,7 +10,6 @@
             </el-main>
         </el-container>
     </div>
-    <ConfirmDialog v-model="activeDialog" :form="form" @cancelDialog="cancelDialog"></ConfirmDialog>
 </template>
 
 <script setup>
